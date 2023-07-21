31a049f5141b31060e5df275d1b7bdfc369e337f
Agyeiwaa <136027473+Agyeiwaa9@users.noreply.github.com>
2023-07-21 19:58:39 +0000

    Update 1-edges.py

--- 1-edges.py
+++ 1-edges.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 word = "Holberton"
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-print("First 3 letters: {}".format(word_first_3))
+string ="Holberton"
+word_first_3 = string[0:2]
+ print("First 3 letters: {}".format(word_first_3))
+word_last_2 = string[7:8]
 print("Last 2 letters: {}".format(word_last_2))
+middle_word = string[1:7]
 print("Middle word: {}".format(middle_word))
