59c915dcb9555e1ad87186f005c70e6021a142e5
Agyeiwaa <136027473+Agyeiwaa9@users.noreply.github.com>
2023-07-21 20:14:01 +0000

    Update 1-edges.py

--- 1-edges.py
+++ 1-edges.py
@@ -4,6 +4,6 @@ string ="Holberton"
 word_first_3 = string[0:3]
  print("First 3 letters: ", word_first_3)
 word_last_2 = string[7:8]
-print("Last 2 letters: {}", word_last_2)
+print("Last 2 letters: ", word_last_2)
 middle_word = string[1:8]
 print("Middle word: ", middle_word)
