3ebaf5f8d5ee3bd796f121c6c7a46a657ddaba2f
Agyeiwaa <136027473+Agyeiwaa9@users.noreply.github.com>
2023-07-21 20:13:34 +0000

    Update 1-edges.py

--- 1-edges.py
+++ 1-edges.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 word = "Holberton"
 string ="Holberton"
-word_first_3 = string[0:2]
- print("First 3 letters: {}".format(word_first_3))
+word_first_3 = string[0:3]
+ print("First 3 letters: ", word_first_3)
 word_last_2 = string[7:8]
-print("Last 2 letters: {}".format(word_last_2))
-middle_word = string[1:7]
-print("Middle word: {}".format(middle_word))
+print("Last 2 letters: {}", word_last_2)
+middle_word = string[1:8]
+print("Middle word: ", middle_word)
