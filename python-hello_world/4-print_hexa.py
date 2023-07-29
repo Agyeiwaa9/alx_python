d91296b792bb90140c071ebc2b8bb4d499d406c3
Agyeiwaa <136027473+Agyeiwaa9@users.noreply.github.com>
2023-07-29 19:13:17 +0000

    Update 4-print_hexa.py

--- python-hello_world/4-print_hexa.py
+++ python-hello_world/4-print_hexa.py
@@ -1,6 +1,9 @@
 def main():
     for num in range(99):
         print("Decimal: {}, Hexadecimal: {:X}".format(num, num))
+        for num in range(99):
+    print(f"{num} = 0x{num:x}")
+
 
 if __name__ == "__main__":
     main()
