e11d8553752613d422190ed1b77e4997e62cf379
Agyeiwaa <136027473+Agyeiwaa9@users.noreply.github.com>
2023-08-02 17:47:55 +0000

    Update 1-power.py

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 pow = __import__('8-pow').pow
 
-def pow(a,b)
+
 print(pow(-2, 2))
 print(pow(2, 2))
 print(pow(-98, -10))
 print(pow(10, -2))
 print(pow(-4, 5))
 
-if __name__ == "__pow__":
-    pow()
+
 
