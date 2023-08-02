aa4cbe8bb8a6f9c38d76fdaf1b3aff7c823897f3
Agyeiwaa <136027473+Agyeiwaa9@users.noreply.github.com>
2023-08-02 14:37:17 +0000

    Update 3-string.py

--- python-functions/3-string.py
+++ python-functions/3-string.py
@@ -1,1 +1,10 @@
+reverse_string = __import__('3-string').reverse_string
+​
+def reverse_string(string)
 
+print(reverse_string("Hello"))
+print(reverse_string(""))
+print(reverse_string("madam"))
+print(reverse_string("Hello, World!"))
+
+reverse_string()
