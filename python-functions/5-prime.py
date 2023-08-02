6b205b2f867edb6b38d4bf804ac17983fdf5589b
Agyeiwaa <136027473+Agyeiwaa9@users.noreply.github.com>
2023-08-02 14:52:26 +0000

    Update 5-prime.py

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,1 +1,13 @@
+!/usr/bin/env python3
+ is_prime = __import__('5-prime').is_prime
+
+def is_prime(number)
+​
+print(is_prime(17))
+print(is_prime(15))
+print(is_prime(-5))
+print(is_prime(0))
+
+if __name__=="__is_prime__":
+  __is_prime__()
 
