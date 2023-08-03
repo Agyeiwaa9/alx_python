dd34ebd4da8a1fe4be04563ab735de7a98f0f096
Agyeiwaa <136027473+Agyeiwaa9@users.noreply.github.com>
2023-08-03 12:16:07 +0000

    Update 2-temperature.py

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,11 +1,14 @@
 __import__('2-temperature').convert_to_celsius
+def convert_to_celsius(fahrenheit):
+    a=100
+    b=-40
+    c=-459.67
+    d=32
 
-def convert_to_celsius(fahrenheit)
-
-print(convert_to_celsius(100))
-print(convert_to_celsius(-40))
-print(convert_to_celsius(-459.67))
-print(convert_to_celsius(32))
+print(convert_to_celsius(a))
+print(convert_to_celsius(b))
+print(convert_to_celsius(c))
+print(convert_to_celsius(d))
 
 if __name__ == "__convert_to_celsius__":
     convert_to_celsius()
