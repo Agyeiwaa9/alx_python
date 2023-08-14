f578e4925de9c1ec6d890efcf7685d07b5c8cb71
Agyeiwaa <136027473+Agyeiwaa9@users.noreply.github.com>
2023-08-14 14:31:12 +0000

    Update 0-add.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -13,10 +13,12 @@ add(a,b) = a + b
 "a = 1"
 "b = 2"
 
-    from add_0 import add
-    result = add(a,b)
-    return (a + b)
-    print("{} + {} = {}".format(a, b, result)
+from add_0 import add
+result = add(a,b)
+return (a + b)
+print("{} + {} = {}".format(a, b, result)
+
+FAKE add()= a-b
 
     
 if ___name___="___main___":
