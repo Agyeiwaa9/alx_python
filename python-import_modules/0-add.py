634a2988a4061caa785dd538284bb399edc82f87
Agyeiwaa <136027473+Agyeiwaa9@users.noreply.github.com>
2023-08-15 15:05:06 +0000

    Update 0-add.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -28,7 +28,7 @@ from add_0 import add
 result = add(a, b)
 result2 = subtract(a, b)
 
-print("{} + {} = {}\n".format(a, b, result))
+print("{}+{} = {}".format(a, b, result))
 
 if __name__ == "__main__":
     pass  # Add your main code here if needed
