bad139be89e4af1d5fdd6a3514b385033d04ad0b
Agyeiwaa <136027473+Agyeiwaa9@users.noreply.github.com>
2023-08-15 15:43:14 +0000

    Update 5-raise_exception_msg.py

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -1,1 +1,18 @@
+def raise_exception_msg(message=""):
+    class Main:
+        def __init__(self, message):
+            self.message = message
+        
+        def __repr__(self):
+            return self.message
+    
+    if __name__ == "__main__":
+        raise Main(message)
+
+try:
+    raise_exception_msg("C is fun")
+except Main as m:
+    result = m
+
+print(result)
 
