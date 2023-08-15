bbad4d1909f77627a622c080835e8816ba82d178
Agyeiwaa <136027473+Agyeiwaa9@users.noreply.github.com>
2023-08-15 15:01:45 +0000

    Update 0-add.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python3
+
 def add(a, b):
+    """My addition function
+    Args:
+        a: First operand
+        b: Second operand
+    Returns:
+        The sum of a and b
+    """
     return a + b
-    
+
 def subtract(a, b):
+    """My subtraction function
+    Args:
+        a: First operand
+        b: Second operand
+    Returns:
+        The difference between a and b
+    """
     return a - b
 
 a = 1
@@ -12,7 +27,8 @@ from add_0 import add
 
 result = add(a, b)
 result2 = subtract(a, b)
-print("{} - {} = {}\n".format(a, b, result2))
+
+print("{} + {} = {}\n{} - {} = {}\n".format(a, b, result, a, b, result2))
 
 if __name__ == "__main__":
-   pass  # Add your main code here if needed
+    pass  # Add your main code here if needed
