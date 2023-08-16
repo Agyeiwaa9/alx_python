f1dde5c05b8ed4a027f60fb00224fd067a422fd6
Agyeiwaa <136027473+Agyeiwaa9@users.noreply.github.com>
2023-08-16 12:21:55 +0000

    Update 1-args.py

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -11,7 +11,7 @@ def print_arguments():
     
     print(args_str)
     for i, arg in enumerate(argv[1:], start=1):
-        print(i,"   :",arg)
+        print(i":",arg)
 
 if __name__ == "__main__":
     print_arguments()
