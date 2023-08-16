24e3b33f5c1af41dc61a4b90b6c0e1e3b3a6e142
Agyeiwaa <136027473+Agyeiwaa9@users.noreply.github.com>
2023-08-16 17:17:34 +0000

    Update 3-safe_print_division.py

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,1 +1,12 @@
+def safe_print_division(a, b):
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+        return result
+
+# Test the function
+result = safe_print_division(10, 2)
 
