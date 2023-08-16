2a486fc9dd164f278ae9ae21b24395f2cb507704
Agyeiwaa <136027473+Agyeiwaa9@users.noreply.github.com>
2023-08-16 12:18:05 +0000

    Update 1-args.py

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -2,12 +2,12 @@ def print_arguments():
     from sys import argv
     
     num_args = len(argv) - 1
-    if num_args == 0:
+    if num_args ==0:
         args_str = '0 arguments.'
-    elif num_args == 1:
+    elif num_args ==1:
         args_str = '1 argument:'
     else:
-        args_str = f"{num_args} arguments:"
+        args_str = f"{num_args}arguments:"
     
     print(args_str)
     for i, arg in enumerate(argv[1:], start=1):
