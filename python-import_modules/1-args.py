bbb2a04db7ae3b6fc4209e9712cc892312796920
Agyeiwaa <136027473+Agyeiwaa9@users.noreply.github.com>
2023-08-16 11:36:42 +0000

    Update 1-args.py

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -3,17 +3,16 @@ def print_arguments():
     
     num_args = len(argv) - 1
     if num_args == 0:
-        args_str = '.'
+        args_str = 'arguments:'
     elif num_args == 1:
         args_str = 'argument:'
     else:
         args_str = 'arguments:'
     
-    print("Number of argument(s) followed by arguments", args_str)
+    print(num_args, args_str)
     if num_args > 0:
         for i, arg in enumerate(argv[1:], start=1):
             print(i, ":", arg)
 
 if __name__ == "__main__":
     print_arguments()
-
