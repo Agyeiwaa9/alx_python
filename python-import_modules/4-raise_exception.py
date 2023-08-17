835de0001d638bd5cd433d1ac1b6bb179adc59e5
Agyeiwaa <136027473+Agyeiwaa9@users.noreply.github.com>
2023-08-17 16:45:39 +0000

    Update 4-raise_exception.py

--- python-import_modules/4-raise_exception.py
+++ python-import_modules/4-raise_exception.py
@@ -1,1 +1,11 @@
+def raise_exception():
+    value = "Hello, world!"
+    if not isinstance(value, int):
+        raise TypeError("Expected an integer, but got: " + str(value))
+
+# Call the function to see the exception in action
+try:
+    raise_exception()
+except TypeError as e:
+    print("Caught an exception:", e)
 
