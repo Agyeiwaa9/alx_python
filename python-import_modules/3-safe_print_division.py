33526746ef86173a3d890d11add70778d0b256e3
Agyeiwaa <136027473+Agyeiwaa9@users.noreply.github.com>
2023-08-17 10:25:13 +0000

    Update 3-safe_print_division.py

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -6,8 +6,6 @@ def safe_print_division(a, b):
     finally:
         if result is not None:
             print("Inside result:", result)
-            if b == 0:
+            if b != 0:
                 print("{} / {} = {}".format(a, b, result))
-        elif b == 0:
-            print("{} / {} = {}".format(a, b, result))
 
