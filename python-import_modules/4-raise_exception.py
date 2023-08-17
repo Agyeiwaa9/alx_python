9d32542c2cc12f5c1aea1161ef6b73ae0081da47
Agyeiwaa <136027473+Agyeiwaa9@users.noreply.github.com>
2023-08-17 17:04:03 +0000

    Update 4-raise_exception.py

--- python-import_modules/4-raise_exception.py
+++ python-import_modules/4-raise_exception.py
@@ -3,5 +3,5 @@ def raise_exception():
     try:
         result = 10 + value  # This will raise a TypeError
     except TypeError:
-        print("Excection has been raised", end='')
+        print("Exception has been raised", end='')
 
