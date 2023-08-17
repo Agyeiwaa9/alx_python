c59601c7fdce39c9dfbfa2eba90aba97f134d4bd
Agyeiwaa <136027473+Agyeiwaa9@users.noreply.github.com>
2023-08-17 17:32:24 +0000

    Update 3-safe_print_division.py

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -4,11 +4,10 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         print("Inside result: None")
         print("{} / {} = None".format(a, b))
-        return None
     except Exception as e:
         print("An error occurred:", str(e))
         print("Inside result: None")
-        return None
+
     finally:
         if 'result' in locals():
             print("Inside result: {}".format(result))
