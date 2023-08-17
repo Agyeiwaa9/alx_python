b3bc310cb172c23ca36889eff519642719268b15
Agyeiwaa <136027473+Agyeiwaa9@users.noreply.github.com>
2023-08-17 10:55:40 +0000

    Update 3-safe_print_division.py

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -4,8 +4,6 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         result = None
     finally:
-        if result is not None:
-            print("Inside result:", result)
             if b != 0:
                 print("{} / {} = {}".format(a, b, result))
         elif b == 0:
