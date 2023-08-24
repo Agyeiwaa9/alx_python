c7e58f351e648f56ccba5974a21fd22e18d73205
Agyeiwaa <136027473+Agyeiwaa9@users.noreply.github.com>
2023-08-24 15:31:13 +0000

    Update 1-power.py

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,11 +1,13 @@
-#!/usr/bin/python3
-def pow (a, b):
+def pow(a, b):
+    if b == 0:
+        return 1
     result = 1
-    for _ in range(b):
-      result *= a
+    if b < 0:
+        a = 1 / a
+        b = -b
+    while b > 0:
+        if b % 2 == 1:
+            result *= a
+        a *= a
+        b //= 2
     return result
-
-
-
-
-
