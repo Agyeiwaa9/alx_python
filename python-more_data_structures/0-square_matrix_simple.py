7d9530cd23c0ed87412c9f5266f428ee6a09b812
Agyeiwaa <136027473+Agyeiwaa9@users.noreply.github.com>
2023-08-26 15:59:23 +0000

    Update 0-square_matrix_simple.py

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,9 @@
-def print_matrix_integer(matrix=[[]], square_matrix_simple=False):
+def print_matrix_integer(matrix=[[]]):
+    print_matrix_integer.square_matrix_simple = True  # Adding the attribute
     for row in matrix:
         print("[", end="")
         for index, num in enumerate(row):
-            if square_matrix_simple:
-                print("{:d}".format(num ** 2), end=", " if index < len(row) - 1 else "")
-            else:
-                print("{:d}".format(num), end=", " if index < len(row) - 1 else "")
+            print("{:d}".format(num ** 2), end="")
+            if index < len(row) - 1:
+                print(", ", end="")
         print("]")
