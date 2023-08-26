8d54cfc827807de22bb69ae2c98c6fa6c3e05410
Agyeiwaa <136027473+Agyeiwaa9@users.noreply.github.com>
2023-08-26 19:20:29 +0000

    Update 2-update_dictionary.py

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -1,8 +1,2 @@
 def update_dictionary(a_dictionary, key, value):
     a_dictionary[key] = value
-update_dictionary(a_dictionary, key, value)
-for k, v in a_dictionary.items():
-    print(f"{k}: {v}")
-print("xx")
-for k, v in a_dictionary.items():
-    print(f"{k}: {v}")
