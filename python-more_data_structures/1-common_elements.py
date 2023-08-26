1ea5e321c2510b3495cc7490d35943d1f26f2d83
Agyeiwaa <136027473+Agyeiwaa9@users.noreply.github.com>
2023-08-26 15:17:23 +0000

    Update 1-common_elements.py

--- python-more_data_structures/1-common_elements.py
+++ python-more_data_structures/1-common_elements.py
@@ -1,1 +1,8 @@
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for col_idx, value in enumerate(row):
+            if col_idx != 0:
+                print(" ", end="")
+            print("{:d}".format(value), end="")
+        print()
 
