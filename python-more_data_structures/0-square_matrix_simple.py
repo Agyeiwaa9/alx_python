cde88fe839ff3c0d5d31ebe95713f3e2701618a7
Agyeiwaa <136027473+Agyeiwaa9@users.noreply.github.com>
2023-08-26 15:54:25 +0000

    Update 0-square_matrix_simple.py

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,9 @@
-def print_matrix_integer(matrix=[[]]):
+def print_matrix_integer(matrix=[[]], square_matrix_simple=False):
     for row in matrix:
         print("[", end="")
-        for i, num in enumerate(row):
-            print("{:d}".format(num ** 2), end="")
-            if i < len(row) - 1:
-                print(", ", end="")
+        for index, num in enumerate(row):
+            if square_matrix_simple:
+                print("{:d}".format(num ** 2), end=", " if index < len(row) - 1 else "")
+            else:
+                print("{:d}".format(num), end=", " if index < len(row) - 1 else "")
         print("]")
