b6556d39d3aeb58708bfdd35dcdb3d72f16c2112
Agyeiwaa <136027473+Agyeiwaa9@users.noreply.github.com>
2023-08-26 17:58:17 +0000

    Update 1-print_matrix_integer.py

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -2,4 +2,4 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for num in row:
             print("{:d}" .format(num), end=" ")
-        print("{:18}".format(""), end="\n")
+        print("{:}" .format(""),end="\n")
