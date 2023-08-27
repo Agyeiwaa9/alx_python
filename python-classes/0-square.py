c441813e5521e3fc1caff79d62a23a4b6c6c259f
Agyeiwaa <136027473+Agyeiwaa9@users.noreply.github.com>
2023-08-27 15:39:23 +0000

    Update 0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,1 +1,8 @@
+class Square:
+    def __init__(self, size):
+        self.__size = size
+
+# Example usage
+square = Square(5)
+print(square._Square__size)  # Accessing private attribute (not recommended)
 
